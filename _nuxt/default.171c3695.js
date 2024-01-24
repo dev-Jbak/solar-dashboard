af89b69c23c587cdd479ce60b4482b96e569ca66
Jack Baker <Jack.daniel.baker@gmail.com>
2024-01-24 19:48:42 +0000

    Updates

--- _nuxt/default.171c3695.js
+++
@@ -1,1 +0,0 @@
-import{_ as s,k as t,l as o,K as a}from"./entry.2fa624c6.js";const c={},r={class:"max-w-screen-3xl h-full m-auto"};function l(e,n){return t(),o("div",r,[a(e.$slots,"default")])}const f=s(c,[["render",l]]);export{f as default};
